351a23982f15b3ded709d34024a98675c5bbedaa
devjunayed <devjunayed@gmail.com>
2024-10-19 21:38:42 +0600

    redux is removed

--- src/app/lib/Providers.tsx
+++ src/app/lib/Providers.tsx
@@ -1,18 +1,10 @@
 import { NextUIProvider } from "@nextui-org/react";
 import React, { ReactNode, useRef } from "react";
-import { AppStore, makeStore } from "../redux/store";
-import { Provider } from "react-redux";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
+  
   return (
-    <Provider store={storeRef.current}>
       <NextUIProvider>{children}</NextUIProvider>
-    </Provider>
   );
 };
 
